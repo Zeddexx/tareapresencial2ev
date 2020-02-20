6a93700afb23839490688350f22dc629f68cf516
Zeddexx <Zeddexx@Zeddexx-Msi>
2020-02-20 17:13:12 +0100

    Esta es la primera version

---
+++ src/tareapresencial2ev/CaminoBasico.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareapresencial2ev;
+
+/**
+ *
+ * @author Zeddexx
+ */
+public class CaminoBasico {
+
+    public int miMetodo(int a, int b) {
+        int resultado = 0;
+        if (a > 5) {
+            resultado = 55;
+        } else if (b < 10) {
+            resultado = 100;
+        }
+        return resultado;
+    }
+}
+
